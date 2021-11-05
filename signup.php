dfc043423ffc69ae0030738cb65298b5fffc986d
iraklikhonelidze <62649197+iraklikhonelidze@users.noreply.github.com>
2021-11-05 12:35:03 +0400

    Added placeholders

--- signup.php
+++ signup.php
@@ -77,8 +77,8 @@ session_start();
 		<form method="post">
 			<div style="font-size: 20px;margin: 10px;color: white;">Signup</div>
 
-			<input id="text" type="text" name="user_name"><br><br>
-			<input id="text" type="password" name="password"><br><br>
+			<input id="text" type="text" name="user_name" placeholder="Username"><br><br>
+			<input id="text" type="password" name="password"placeholder="Password"><br><br>
 
 			<input id="button" type="submit" value="Signup"><br><br>
 
